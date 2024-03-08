e32bb23da32f87cb7605399fc5074bb87fd08ed4
efesxzc <rav4xzc@gmail.com>
2024-03-08 16:59:56 +0500

    очистка

--- main.cpp
+++
@@ -1,9 +0,0 @@
-#include "lib.h"
-
-#include <iostream>
-
-int main (int, char **) {
-    std::cout << "Version: " << version() << std::endl;
-    std::cout << "Hello, wooorld!  " << std::endl;
-    return 0;
-}
